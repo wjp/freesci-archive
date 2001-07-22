7a06b7122da37a21e0e31d83f2edb0999ece87f7
Solomon Peachy <pizza@shaftnet.org>
2001-07-22 19:43:07 +0000

    Lot's changed.
    Thanks to Ravi's latest specs, the adlib instruments are now loaded
    properly and don't make your ears bleed.
    
    It now pumps out something resembling music, but with a few problems:
    
    1) timing is a little shot.
    2) operator/voice allocation
    
    (1) I suspect is due to OSSseq not being all that great for realtime
        events.  I'm going to give alsa's sequencer a spin and see how that
        compares.
    (2) We need to open up the channel priority to the midi_device layer.
        I'll probably tackle this later today.
    
     - Pizza

--- src/sound/midi_adlib.c
+++ src/sound/midi_adlib.c
@@ -17,11 +17,11 @@
 
 ***************************************************************************/
 
-/* TODO:  Get the insturments working. 
-          Move everything from a 'channel' mentality to a 'note' mentality.
+/* TODO: 
           Global volume.
 	  Pitch bend.
-	  allstop.
+	  Timing issues.
+	  ALSA?
 */
 
 #include <stdio.h>
@@ -38,35 +38,59 @@
 #include <sys/ioctl.h>
 #include <sys/soundcard.h>
 
+#define ADLIB_VOICES 9
 
-/*SEQ_DEFINEBUF(1024);*/
-extern unsigned char _seqbuf[1024];
+#if 0
+SEQ_DEFINEBUF(2048);
+static int seqfd;
+#else
+extern unsigned char _seqbuf[2048];
 extern int _seqbuflen;
 extern int _seqbufptr;
+extern int seqfd;
+#endif
 
 typedef struct _sci_adlib_def {
-  guint8 keyscale;       /* 0-3 !*/
-  guint8 freqmod;        /* 0-15 !*/
-  guint8 feedback;       /* 0-7 !*/
-  guint8 attackrate;     /* 0-15 !*/
-  guint8 sustainvol;     /* 0-15 !*/
-  guint8 envelope;       /* t/f !*/
-  guint8 decayrate;      /* 0-15 !*/
-  guint8 releaserate;    /* 0-15 !*/
-  guint8 volume;         /* 0-63 !*/
-  guint8 ampmod;         /* t/f !*/
-  guint8 vibrato;        /* t/f !*/
-  guint8 keybdscale;     /* t/f !*/
-  guint8 algorithm;      /* 0,1 !*/
-  guint8 waveform;       /* 0-3 !*/
+  guint8 keyscale1;       /* 0-3 !*/
+  guint8 freqmod1;        /* 0-15 !*/
+  guint8 feedback1;       /* 0-7 !*/
+  guint8 attackrate1;     /* 0-15 !*/
+  guint8 sustainvol1;     /* 0-15 !*/
+  guint8 envelope1;       /* t/f !*/
+  guint8 decayrate1;      /* 0-15 !*/
+  guint8 releaserate1;    /* 0-15 !*/
+  guint8 volume1;         /* 0-63 !*/
+  guint8 ampmod1;         /* t/f !*/
+  guint8 vibrato1;        /* t/f !*/
+  guint8 keybdscale1;     /* t/f !*/
+  guint8 algorithm1;      /* 0,1 REVERSED? */
+  guint8 keyscale2;       /* 0-3 !*/
+  guint8 freqmod2;        /* 0-15 !*/
+  guint8 feedback2;       /* 0-7 UNUSED? */
+  guint8 attackrate2;     /* 0-15 !*/
+  guint8 sustainvol2;     /* 0-15 !*/
+  guint8 envelope2;       /* t/f !*/
+  guint8 decayrate2;      /* 0-15 !*/
+  guint8 releaserate2;    /* 0-15 !*/
+  guint8 volume2;         /* 0-63 !*/
+  guint8 ampmod2;         /* t/f !*/
+  guint8 vibrato2;        /* t/f !*/
+  guint8 keybdscale2;     /* t/f !*/
+  guint8 algorithm2;      /* 0,1 UNUSED? */
+  guint8 waveform1;       /* 0-3 !*/
+  guint8 waveform2;       /* 0-3 !*/
 } adlib_def;
 
 static sbi_instr_data adlib_sbi[96];
 static guint8 instr[MIDI_CHANNELS];
-static int seqfd, dev;
+static int polyphony[MIDI_CHANNELS];
+static int dev;
+static int free_voices = ADLIB_VOICES;
+static long note_time[ADLIB_VOICES];
+static unsigned char oper_note[ADLIB_VOICES];
+static unsigned char oper_chn[ADLIB_VOICES];
 
 #if 0
-/* -------------- */
 void seqbuf_dump()
 {
   if (_seqbufptr)
@@ -76,53 +100,163 @@ void seqbuf_dump()
     }
   _seqbufptr = 0;
 }
-/* -------------- */
 #endif
 
-void make_sbi(adlib_def *one, adlib_def *two, guint8 *buffer)
+void make_sbi(adlib_def *one, guint8 *buffer)
 {
   memset(buffer, 0, sizeof(sbi_instr_data));
 
-  printf ("%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x ", one->keyscale, one->freqmod, one->feedback, one->attackrate, one->sustainvol, one->envelope, one->decayrate, one->releaserate, one->volume, one->ampmod, one->vibrato, one->keybdscale, one->algorithm, one->waveform);
+#if 0
+ printf ("%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x ", one->keyscale1, one->freqmod1, one->feedback1, one->attackrate1, one->sustainvol1, one->envelope1, one->decayrate1, one->releaserate1, one->volume1, one->ampmod1, one->vibrato1, one->keybdscale1, one->algorithm1, one->waveform1);
 
-  printf (" %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x ", two->keyscale, two->freqmod, two->feedback, two->attackrate, two->sustainvol, two->envelope, two->decayrate, two->releaserate, two->volume, two->ampmod, two->vibrato, two->keybdscale, two->algorithm, two->waveform);
+  printf (" %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x ", one->keyscale2, one->freqmod2, one->feedback2, one->attackrate2, one->sustainvol2, one->envelope2, one->decayrate2, one->releaserate2, one->volume2, one->ampmod2, one->vibrato2, one->keybdscale2, one->algorithm2, one->waveform2);
 
   printf("\n");
+#endif
 
-  buffer[0] |= ((one->ampmod & 0x1) << 7);
-  buffer[0] |= ((one->vibrato & 0x1) << 6);
-  buffer[0] |= ((one->envelope & 0x1) << 5);
-  buffer[0] |= ((one->keybdscale & 0x1) << 4);
-  buffer[0] |= (one->freqmod & 0xf);
-  buffer[1] |= ((two->ampmod & 0x1) << 7);
-  buffer[1] |= ((two->vibrato & 0x1) << 6);
-  buffer[1] |= ((two->envelope & 0x1) << 5);
-  buffer[1] |= ((two->keybdscale & 0x1) << 4);
-  buffer[1] |= (two->freqmod & 0xf);
-  buffer[2] |= ((one->keyscale & 0x3) << 6);
-  buffer[2] |= (one->volume & 0x3f);
-  buffer[3] |= ((two->keyscale & 0x3) << 6); 
-  buffer[3] |= (two->volume & 0x3f);
-  buffer[4] |= ((one->attackrate & 0xf) << 4);
-  buffer[4] |= (one->decayrate & 0xf);  
-  buffer[5] |= ((two->attackrate & 0xf) << 4);
-  buffer[5] |= (two->decayrate & 0xf);  
-  buffer[6] |= ((one->sustainvol & 0xf) << 4);
-  buffer[6] |= (one->releaserate & 0xf);
-  buffer[7] |= ((two->sustainvol & 0xf) << 4);
-  buffer[7] |= (two->releaserate & 0xf);
-  buffer[8] |= (one->waveform & 0x3);
-  buffer[9] |= (two->waveform & 0x3);
+  buffer[0] |= ((one->ampmod1 & 0x1) << 7);
+  buffer[0] |= ((one->vibrato1 & 0x1) << 6);
+  buffer[0] |= ((one->envelope1 & 0x1) << 5);
+  buffer[0] |= ((one->keybdscale1 & 0x1) << 4);
+  buffer[0] |= (one->freqmod1 & 0xf);
+  buffer[1] |= ((one->ampmod2 & 0x1) << 7);
+  buffer[1] |= ((one->vibrato2 & 0x1) << 6);
+  buffer[1] |= ((one->envelope2 & 0x1) << 5);
+  buffer[1] |= ((one->keybdscale2 & 0x1) << 4);
+  buffer[1] |= (one->freqmod2 & 0xf);
+  buffer[2] |= ((one->keyscale1 & 0x3) << 6);
+  buffer[2] |= (one->volume1 & 0x3f);
+  buffer[3] |= ((one->keyscale2 & 0x3) << 6); 
+  buffer[3] |= (one->volume2 & 0x3f);
+  buffer[4] |= ((one->attackrate1 & 0xf) << 4);
+  buffer[4] |= (one->decayrate1 & 0xf);  
+  buffer[5] |= ((one->attackrate2 & 0xf) << 4);
+  buffer[5] |= (one->decayrate2 & 0xf);  
+  buffer[6] |= ((one->sustainvol1 & 0xf) << 4);
+  buffer[6] |= (one->releaserate1 & 0xf);
+  buffer[7] |= ((one->sustainvol2 & 0xf) << 4);
+  buffer[7] |= (one->releaserate2 & 0xf);
+  buffer[8] |= (one->waveform1 & 0x3);
+  buffer[9] |= (one->waveform2 & 0x3);
 
-  buffer[10] |= ((one->feedback & 0x7) << 1);
-  buffer[10] |= (one->algorithm & 0x1);
-
-  /*
-  buffer[10] |= ((two->feedback & 0x7) << 1);
-  buffer[10] |= (two->algorithm & 0x1);
-  */
+  buffer[10] |= ((one->feedback1 & 0x7) << 1);
+  buffer[10] |= (1-(one->algorithm1 & 0x1));
 
   return;
+}
+
+/* initialise note/operator lists, etc. */
+void adlib_init_lists()
+{
+  int i;
+  for(i = 0 ; i < ADLIB_VOICES ; i++) {
+    oper_note[i] = 255;
+    oper_chn[i] = 255;
+    note_time[i] = 0;
+  }
+};
+
+int adlib_stop_note(int chn, int note, int velocity)
+{
+  int i, op=255;
+  
+  for (i=0;i<ADLIB_VOICES && op==255;i++) {
+    //    if (oper_chn[i] == chn)
+      if (oper_note[i] == note)
+	op=i;
+  }
+
+  if (op==255) {
+    printf ("can't stop.. chn %d %d %d\n", chn, note, velocity);
+    return 255;	/* not playing */
+  }
+  
+  SEQ_STOP_NOTE(dev, op, note, velocity);
+  SEQ_DUMPBUF();
+
+  oper_chn[op] = 255;
+  oper_note[op] = 255;
+  note_time[op] = 0;
+  
+  free_voices++;
+
+  return op;
+}
+
+int adlib_kill_one_note(int chn)
+{
+  int oldest, i = 255;
+  long time = 0;
+     
+  if (free_voices >= ADLIB_VOICES) {
+    printf("Free list empty but no notes playing\n"); 
+    return;
+  }	/* No notes playing */
+
+  for (i = 0; i < ADLIB_VOICES ; i++) {
+    /*
+      if (oper_chn[i] != chn)
+      continue;
+    */
+    if (note_time[i] == 0)
+      continue;
+    if (time == 0) {
+      time = note_time[i];
+      oldest = i;
+      continue;
+    }
+    if (note_time[i] < time) {
+      time = note_time[i];
+      oldest = i;
+    }
+  }
+
+  printf("Killing chn %d, oper %d\n", chn, oldest);
+
+  if (oldest == 255) 
+    return 255;	/* Was already stopped. Why? */
+
+  SEQ_STOP_NOTE(dev, oldest, oper_note[oldest], 0);
+  SEQ_DUMPBUF();
+
+  oper_chn[oldest] = 255;
+  oper_note[oldest] = 255;
+  note_time[oldest] = 0;
+  free_voices++;
+
+  return oldest;
+}
+
+void adlib_start_note(int chn, int note, int velocity)
+{
+  int free;    
+  struct timeval now;
+
+  if (velocity == 0) {
+    adlib_stop_note(chn, note, velocity);
+    return;
+  }
+
+  gettimeofday(&now, NULL);  
+
+  if (free_voices <= 0)
+    free = adlib_kill_one_note(chn);
+  else 
+    for (free = 0; free < ADLIB_VOICES ; free++)
+      if (oper_chn[free] == 255)
+	break;
+
+  printf("play operator %d/%d:  %d %d %d\n", free, free_voices, chn, note, velocity);
+
+  oper_chn[free] = chn;
+  oper_note[free] = note;
+  note_time[free] = now.tv_sec * 1000000 + now.tv_usec;
+  free_voices--;
+  
+  SEQ_SET_PATCH(dev, free, instr[chn]);
+  //XXXX  SEQ_PITCHBEND(dev, free, bender);
+  SEQ_START_NOTE(dev, free, note, velocity);
+  SEQ_DUMPBUF();
 }
 
 int midi_adlib_open(guint8 *data_ptr, unsigned int data_length)
@@ -137,11 +271,11 @@ int midi_adlib_open(guint8 *data_ptr, unsigned int data_length)
   }
 
   for (i = 0; i < 48; i++) 
-    make_sbi(data_ptr+(28 * i), data_ptr+14+(28 * i), adlib_sbi[i]);
+    make_sbi(data_ptr+(28 * i), adlib_sbi[i]);
 
   if (data_length > 1344)
     for (i = 48; i < 96; i++) 
-      make_sbi(data_ptr+2+(28 * i), data_ptr+2+14+(28 * i), adlib_sbi[i]);
+      make_sbi(data_ptr+2+(28 * i), adlib_sbi[i]);
 
   memset(instr, 0, sizeof(instr));
  
@@ -166,6 +300,9 @@ int midi_adlib_open(guint8 *data_ptr, unsigned int data_length)
     fprintf(stderr, "ADLIB: FM synthesizer not detected\n");
     return(-1);
   }
+
+  /*  free_voices = info.nr_voices; */
+  adlib_init_lists();
 
   printf("ADLIB: Loading patches into synthesizer\n");
   sbi.device = dev;
@@ -198,7 +335,13 @@ int midi_adlib_volume(guint8 volume)
 }
 
 int midi_adlib_allstop(void) {
-  printf("allstop NYI\n");
+  int i;
+  for (i = 0; i < ADLIB_VOICES ; i++) {
+    if (oper_chn[i] == 255)
+      continue;
+    adlib_stop_note(oper_chn[i], oper_note[i], 0);
+  }
+  printf("end allstop\n");
   return 0;
 }
 
@@ -210,15 +353,13 @@ int midi_adlib_reverb(short param)
 
 int midi_adlib_noteoff(guint8 channel, guint8 note, guint8 velocity)
 {
-  SEQ_STOP_NOTE(dev, channel, note, velocity);
-  SEQ_DUMPBUF();
+  adlib_stop_note(channel, note, velocity);
   return 0;
 }
 
 int midi_adlib_noteon(guint8 channel, guint8 note, guint8 velocity)
 {
-  SEQ_START_NOTE(dev, channel, note, velocity);
-  SEQ_DUMPBUF();
+  adlib_start_note(channel,note,velocity);
   return 0;
 }
 
@@ -230,15 +371,14 @@ int midi_adlib_event(guint8 command, guint8 note, guint8 velocity)
   oper = command & 0xf0;
 
   switch (oper) {    
-  case 0x80:  /* XXXX noteon and noteoff */
-    if (velocity != 0)
-      return midi_adlib_noteon(channel, note, velocity);
+  case 0x80:  /* noteon and noteoff */
+    return midi_adlib_noteon(channel, note, velocity);
   case 0x90:
     return midi_adlib_noteoff(channel, note, velocity);
   case 0xe0:    /* Pitch bend NYI */
     break;
-  case 0xb0:    /* CC changes.  let 'em through... */
-    break;
+  case 0xb0:    /* CC changes.  we ignore. */
+    return 0;
   case 0xd0:    /* aftertouch */
     SEQ_CHN_PRESSURE(dev, channel, note);
     SEQ_DUMPBUF();    
@@ -248,7 +388,6 @@ int midi_adlib_event(guint8 command, guint8 note, guint8 velocity)
     return 0;
   }
   
-  printf("event NYI %02x %02x %02x\n", command, note, velocity); 
   SEQ_DUMPBUF();
   return 0;
 }
@@ -265,9 +404,9 @@ int midi_adlib_event2(guint8 command, guint8 param)
   case 0xc0: {  /* change instrument */
     int inst = param;
     instr[channel] = inst; /* XXXX offset? */
-    SEQ_SET_PATCH(dev, channel, inst);
-    SEQ_DUMPBUF();
-    return 0; /* MAGIC */
+    //    SEQ_SET_PATCH(dev, channel, inst);
+    //    SEQ_DUMPBUF();
+    return 0;
   }
   default:
     printf("ADLIB: Unknown event %02x\n", command);
@@ -292,7 +431,7 @@ midi_device_t midi_device_adlib = {
   003,		/* patch.001 */
   0x04,		/* playflag */
   1, 		/* play channel 9 -- rhythm ? */
-  9		/* Max polyphony */
+  ADLIB_VOICES  /* Max polyphony */
 };
 
 #endif /* HAVE_SYS_SOUNDCARD_H */
